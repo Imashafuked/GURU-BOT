0d9a872411d8057011e85aecd7f7210da0fec807
Imashafuked <116446595+Imashafuked@users.noreply.github.com>
2023-05-18 23:00:19 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,8 +7,8 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['94764497079', 'sandaru', true],
-  ['6281255369012', 'Ash', true], 
+  ['94754664570', 'Dhanuja', true],
+  ['94741594855', 'Sadaham', true], 
   [''] 
 ] //Number of owners
 
